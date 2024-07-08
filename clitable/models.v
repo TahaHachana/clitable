b0d5b21c417a10831c00b5ab991d09c2945db73c
TahaHachana <tahahachana@gmail.com>
2024-07-08 07:14:44 +0100

    feat: support setting the padding before and after cells text

--- clitable/models.v
+++ clitable/models.v
@@ -1,32 +1,49 @@
 module clitable
 
 pub struct Column {
-    name string
+	name string
 }
 
 fn Column.new(name string) Column {
-    return Column{name: name}
+	return Column{
+		name: name
+	}
 }
 
 pub struct Row {
-    values []string
-    mut: current_idx int = -1
+	values []string
+mut:
+	current_idx int = -1
 }
 
 fn Row.new(values []string) Row {
-    return Row{values: values}
+	return Row{
+		values: values
+	}
 }
 
 fn (mut r Row) next() ?string {
-    r.current_idx++
-    if r.current_idx >= r.values.len {
-        return none
-    }
-    return r.values[r.current_idx]
+	r.current_idx++
+	if r.current_idx >= r.values.len {
+		return none
+	}
+	return r.values[r.current_idx]
 }
 
 pub struct Table {
 mut:
-    columns []Column
-    rows []Row
+	columns []Column
+	rows    []Row
+	pub mut: padding int = 1
+}
+
+pub fn (mut t Table) add_column(name string) {
+	t.columns << Column.new(name)
+}
+
+pub fn (mut t Table) add_row(values []string) {
+	if t.columns.len != values.len {
+		panic('Number of values does not match number of columns')
+	}
+	t.rows << Row.new(values)
 }
--- clitable/table.v
+++ clitable/table.v
@@ -3,28 +3,17 @@ module clitable
 import arrays
 
 // Box drawing characters
-const box_drawing_light_horizontal = '─'
-const box_drawing_light_vertical = '│'
-const box_drawing_light_down_and_right = '┌'
-const box_drawing_light_down_and_left = '┐'
-const box_drawing_light_up_and_right = '└'
-const box_drawing_light_up_and_left = '┘'
-const box_drawing_light_vertical_and_right = '├'
-const box_drawing_light_vertical_and_left = '┤'
-const box_drawing_light_down_and_horizontal = '┬'
-const box_drawing_light_up_and_horizontal = '┴'
-const box_drawing_light_vertical_and_horizontal = '┼'
-
-pub fn (mut t Table) add_column(name string) {
-	t.columns << Column.new(name)
-}
-
-pub fn (mut t Table) add_row(values []string) {
-	if t.columns.len != values.len {
-		panic('Number of values does not match number of columns')
-	}
-	t.rows << Row.new(values)
-}
+const light_horizontal = '─'
+const light_vertical = '│'
+const light_down_and_right = '┌'
+const light_down_and_left = '┐'
+const light_up_and_right = '└'
+const light_up_and_left = '┘'
+const light_vertical_and_right = '├'
+const light_vertical_and_left = '┤'
+const light_down_and_horizontal = '┬'
+const light_up_and_horizontal = '┴'
+const light_vertical_and_horizontal = '┼'
 
 // Calculate the maximum width of each column
 fn calculate_column_widths(table [][]string) []int {
@@ -40,43 +29,44 @@ fn calculate_column_widths(table [][]string) []int {
 }
 
 // Print a border line
-fn print_border(widths []int, acc string, middle_drawing string, end_drawing string) {
+fn print_border(widths []int, acc string, padding int, middle_drawing string, end_drawing string) {
 	if widths.len == 1 {
-		println(acc + clitable.box_drawing_light_horizontal.repeat(widths[0] + 2) + end_drawing)
+		println(acc + clitable.light_horizontal.repeat(widths[0] + (padding * 2)) + end_drawing)
 	} else {
-		print_border(widths[1..], acc + clitable.box_drawing_light_horizontal.repeat(widths[0] +
-			2) + middle_drawing, middle_drawing, end_drawing)
+		print_border(widths[1..], acc + clitable.light_horizontal.repeat(widths[0] +
+			(padding * 2)) + middle_drawing, padding, middle_drawing, end_drawing)
 	}
 }
 
 // Print the top border of the table
-fn print_top_border(widths []int, acc string) {
-	print_border(widths, acc, clitable.box_drawing_light_down_and_horizontal, clitable.box_drawing_light_down_and_left)
+fn print_top_border(widths []int, acc string, padding int) {
+	print_border(widths, acc, padding, clitable.light_down_and_horizontal, clitable.light_down_and_left)
 }
 
 // Print a separator line between rows
-fn print_separator_line(widths []int, acc string) {
-	print_border(widths, acc, clitable.box_drawing_light_vertical_and_horizontal, clitable.box_drawing_light_vertical_and_left)
+fn print_separator_line(widths []int, acc string, padding int) {
+	print_border(widths, acc, padding, clitable.light_vertical_and_horizontal, clitable.light_vertical_and_left)
 }
 
-fn print_bottom_border(widths []int, acc string) {
-	print_border(widths, acc, clitable.box_drawing_light_up_and_horizontal, clitable.box_drawing_light_up_and_left)
+fn print_bottom_border(widths []int, acc string, padding int) {
+	print_border(widths, acc, padding, clitable.light_up_and_horizontal, clitable.light_up_and_left)
 }
 
 // Create a formatted string for a row based on column widths
-fn format_row(row []string, widths []int) string {
+fn format_row(row []string, widths []int, padding int) string {
 	mut formatted_row := ''
 	for i, cell in row {
-		formatted_row += ' ' + cell + ' '.repeat(widths[i] - cell.len) + ' ' +
-			clitable.box_drawing_light_vertical
+		formatted_row += ' '.repeat(padding) + cell + ' '.repeat(widths[i] - cell.len) +
+			' '.repeat(padding) + clitable.light_vertical
 	}
-	return clitable.box_drawing_light_vertical + formatted_row
+	return clitable.light_vertical + formatted_row
 }
 
 // Print the table
 pub fn print_table(table Table) {
 	columns := table.columns.map(|c| c.name)
 	rows := arrays.append([columns], table.rows.map(|r| r.values))
+	padding := table.padding
 
 	if rows.len == 0 {
 		println('Empty table')
@@ -87,23 +77,23 @@ pub fn print_table(table Table) {
 	widths := calculate_column_widths(rows)
 
 	// Print top border
-	print_top_border(widths, clitable.box_drawing_light_down_and_right)
+	print_top_border(widths, clitable.light_down_and_right, padding)
 
 	// Print header row
-	println(format_row(rows[0], widths))
+	println(format_row(rows[0], widths, padding))
 
 	// Print separator line
-	print_separator_line(widths, clitable.box_drawing_light_vertical_and_right)
+	print_separator_line(widths, clitable.light_vertical_and_right, padding)
 
 	// Print data rows except the last one
 	for row in rows[1..rows.len - 1] {
-		println(format_row(row, widths))
-		print_separator_line(widths, clitable.box_drawing_light_vertical_and_right)
+		println(format_row(row, widths, padding))
+		print_separator_line(widths, clitable.light_vertical_and_right, padding)
 	}
 
 	// Print last row without separator line
-	println(format_row(rows[rows.len - 1], widths))
+	println(format_row(rows[rows.len - 1], widths, padding))
 
 	// Print bottom border
-	print_bottom_border(widths, clitable.box_drawing_light_up_and_right)
+	print_bottom_border(widths, clitable.light_up_and_right, padding)
 }
